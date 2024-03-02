2c49888d61012644ef6d96ab91048da629d7b954
Ajay Rajput <ajaysingh2017rajput@gmail.com>
2024-03-03 01:03:15 +0530

    new project

---
+++ Mern/frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
